5def7fcfe1c1bfb3a1661d51a7f502dc3f95658b
Luke Scott <77630341+Luke270601@users.noreply.github.com>
2022-07-03 20:25:56 +0100

    Added nav bar (unstable build)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './images/img.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="Main">
+            <nav className="navbar">
+                <div className="container-fluid">
+                    <div className="row row-cols-2">
+                        <div className="col-5">
+                            <img className="App-logo" src={logo}/>
+                        </div>
+                        <div className="col-1">
+                            <span className="navbar-brand-font-size">Booking Management</span>
+                        </div>
+                    </div>
+                    <button type="button" className="btn btn-outline-light">Login</button>
+                </div>
+            </nav>
+            <header className="Main-header">
+            </header>
+
+            <input type="text"/>
+            <input type=""/>
+            <input type="text"/>
+            <input type="text"/>
+        </div>
+    );
 }
 
 export default App;
